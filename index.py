2138e0097b514fa8bb9c3a7ec60b932a499ce693
Udoka Amukamara <udokaamukamara@gmail.com>
2021-09-09 16:11:47 +0200

    fixed training method try except

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
+from pathlib import Path
 import numpy
 import tflearn
 import tensorflow
@@ -74,17 +75,15 @@ net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
-model = tflearn.DNN(net)
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
-
-# try:
-#     model.load("model.tflearn")
-#     print("Model exists")
-# except:
-#     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-#     model.save("model.tflearn")
-#     print("Model is being created")
+try:
+    model = tflearn.DNN(net)
+    model.load("model.tflearn")
+    print("Model exists")
+except:
+    model = tflearn.DNN(net)
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+    print("Model is being created")
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
